3114e70e9d483a259ea00343d2ca80bdc44ff75f
BEOMSEOK CHO <bemimer@gmail.com>
2021-12-02 00:01:05 +0900

    temp: feat signup

---
+++ src/components/vivinosignup2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Vivinosignup2 = (props) => {
+  return (
+    <div className="loginModal_contentContainer--2q6yb">
+      <a className="anchor__anchor--2QZvA loginModal__backButton--Z6Rwr">
+        <svg
+          className="chevron__chevron--2zNRX loginModal__chevron--3bqCe chevron__left--2pdho"
+          viewBox="0 0 7 14"
+        >
+          <g>
+            <path
+              d="M-0.000,13.281 L6.568,7.001 L-0.000,0.722 "
+              fill="none"
+              stroke="#A11122"
+              stroke-width="1"
+            ></path>
+          </g>
+        </svg>
+        <span> Back</span>
+      </a>
+
+      <form className="signupForm__signupForm--3365U" novalidate=""><svg class="signupForm__logo--3R_GE" xmlns="http://www.w3.org/2000/svg" width="150" height="240" viewBox="0 0 150 240"><path fill="#A61A30" fill-rule="evenodd" d="M75 182.668c9.12 0 16.685 7.454 16.685 16.665C91.685 208.443 84.12 216 75 216c-9.223 0-16.685-7.556-16.685-16.667 0-9.211 7.462-16.665 16.685-16.665zm18.54-33.333c9.223 0 16.685 7.556 16.685 16.665 0 9.214-7.462 16.666-16.685 16.666-9.224 0-16.686-7.452-16.686-16.666 0-9.109 7.462-16.665 16.686-16.665zM71.91 166c0 9.214-7.462 16.666-16.685 16.666-9.12 0-16.686-7.452-16.686-16.666 0-9.109 7.566-16.665 16.686-16.665 9.223 0 16.685 7.556 16.685 16.665zm24.72-33.333c0-9.11 7.461-16.666 16.684-16.666 9.224 0 16.686 7.555 16.686 16.666 0 9.213-7.462 16.666-16.686 16.666-9.223 0-16.685-7.453-16.685-16.666zM75 149.333c-9.223 0-16.685-7.453-16.685-16.666C58.315 123.556 65.777 116 75 116c9.12 0 16.685 7.555 16.685 16.666 0 9.213-7.566 16.666-16.685 16.666zM36.685 116c9.12 0 16.686 7.555 16.686 16.666 0 9.213-7.566 16.666-16.686 16.666-9.223 0-16.685-7.453-16.685-16.666C20 123.556 27.462 116 36.685 116zM93.54 82.667c9.223 0 16.685 7.58 16.685 16.718 0 9.241-7.462 16.614-16.685 16.614-9.224 0-16.686-7.373-16.686-16.614 0-9.137 7.462-16.718 16.686-16.718zM55.225 116c-9.224 0-16.686-7.373-16.686-16.614 0-9.137 7.462-16.718 16.686-16.718 9.223 0 16.685 7.581 16.685 16.718 0 9.241-7.462 16.614-16.685 16.614zm36.46-50c0 9.11-7.462 16.666-16.685 16.666-9.12 0-16.685-7.557-16.685-16.666 0-9.212 7.565-16.665 16.685-16.665 9.223 0 16.685 7.453 16.685 16.665zm1.854-16.667c-9.12 0-16.685-7.557-16.685-16.666C76.854 23.453 84.419 16 93.539 16c9.224 0 16.686 7.453 16.686 16.666 0 9.109-7.462 16.666-16.686 16.666z"></path></svg><h2 class="signupForm__title--3NqFD">Join Vivino</h2><div class="signupForm__fieldContainer--P1VNV"><input class="Input__input--p7Rzo signupForm__textField--3tfNE Input__sizeMedium--1-1ZA" data-testid="signupFormFirstName" name="fname" placeholder="First name" type="text" value="" spellcheck="false" data-ms-editor="true"></div><div class="signupForm__fieldContainer--P1VNV signupForm__lastName--1vimx"><input class="Input__input--p7Rzo signupForm__textField--3tfNE Input__sizeMedium--1-1ZA" data-testid="signupFormLastName" name="lname" placeholder="Last name" type="text" value="" spellcheck="false" data-ms-editor="true"></div><div class="signupForm__fieldContainer--P1VNV"><input class="Input__input--p7Rzo signupForm__textField--3tfNE signupForm__password--28ctq Input__sizeMedium--1-1ZA" name="password" placeholder="Set a password" required="" type="password" value=""></div><button class="MuiButtonBase-root MuiButton-root jss1 MuiButton-contained signupForm__modalButton--2E_Ry MuiButton-containedPrimary jss2 MuiButton-disableElevation" tabindex="0" type="submit" data-testid="submitButton"><span class="MuiButton-label jss7"><span class="jss11">Join Vivino</span></span></button><div class="signupForm__footer--1tV7R"><h3 class="signupForm__magicLinkTitle--1-k29">Magic Link instead of a password?</h3><p class="signupForm__info--2dn1S">We can email you a Magic Link that will instantly log you in.</p><a class="anchor__anchor--2QZvA signupForm__link--2YmLT">Send me a Magic Link</a></div></form>
+    </div>
+  );
+};
+
+export default Vivinosignup2;
